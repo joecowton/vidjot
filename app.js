c06ce65bde25a5291d0a2055fac30c4a6e64377b
Joe Cowton <joecowton@gmail.com>
2018-01-07 23:45:42 +0000

    item model working;

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const exphbs = require('express-handlebars')
 const mongoose = require('mongoose')
-
+const bodyParser = require('body-parser')
 const app = express();
 
 mongoose.Promise = global.Promise;
@@ -14,10 +14,13 @@ mongoose.connect('mongodb://localhost/vidjot-dev',{
 
 // Load idea model
 require('./models/Idea')
-const Idea = mongoose.model('ideas') 
+const Idea = mongoose.model('ideas')
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}))
 app.set('view engine', 'handlebars')
+
+app.use(bodyParser.urlencoded({ extended: false}))
+app.use(bodyParser.json())
 
 app.get('/',(req, res) => {
   const title = 'Welcome';
@@ -28,6 +31,49 @@ app.get('/',(req, res) => {
 
 app.get('/about',(req, res) => {
   res.render('about')
+})
+
+app.get('/ideas', (req, res) => {
+  Idea.find({})
+    .sort({date:'desc'})
+    .then(ideas => {
+      res.render('ideas/index', {
+        ideas: ideas
+      })
+
+    })
+})
+
+app.get('/ideas/add', (req, res) => {
+  res.render('ideas/add')
+})
+
+app.post('/ideas', (req, res) => {
+  let errors = [];
+
+  if(!req.body.title){
+    errors.push({text:'please add a title'})
+  }
+  if(!req.body.details){
+    errors.push({text:'please add details'})
+  }
+  if(errors.length > 0 ){
+    res.render('ideas/add',{
+      errors: errors,
+      title: req.body.title,
+      details: req.body.details
+    });
+  } else {
+    const newUser = {
+      title: req.body.title,
+      details: req.body.details
+    }
+    new Idea(newUser)
+      .save()
+      .then(idea => {
+        res.redirect('/ideas')
+      })
+  }
 })
 
 const port = 5000;
